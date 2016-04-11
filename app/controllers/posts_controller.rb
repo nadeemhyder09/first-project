a7817baa74ad4c99314c3802e563eea4294a6f76
Nadeem Hyder <nadeemhyder09@gmail.com>
2016-04-11 10:32:10 +0500

    First Commit

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,43 @@
+class PostsController < ApplicationController
+  def index
+    @posts = Post.all
+  end
+  def show
+    @post = Post.find(params[:id])
+  end
+  def new
+    @post = Post.new
+  end
+  def create
+    @post = Post.new(p_params)
+
+    if @post.save 
+      redirect_to posts_path, :notice => "Your post was SAVED"
+    else
+      render "new"
+    end
+  end
+  def edit
+    @post = Post.find(params[:id])
+  end
+  def update
+    @post = Post.find(params[:id])
+    if @post.update_attributes(p_params)
+      redirect_to posts_path, :notice => "Your post was Updated"
+      else
+        render "edit"
+    end
+  end
+  def destroy
+     @post = Post.find(params[:id])
+     @post.destroy
+     redirect_to posts_path, :notice => "Your post has been Deleted"
+  end
+  private
+
+def p_params
+ params.require(:post).permit(:title, :content)
+end
+end
+
+
